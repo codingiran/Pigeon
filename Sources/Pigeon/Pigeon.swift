6c851fbdf0d0b1974f44ce05372c5feeec2b44cb
CodingIran <codingiran@gmail.com>
2024-10-24 21:56:41 +0800

    Release 0.0.7

--- Sources/Pigeon/Pigeon.swift
+++ Sources/Pigeon/Pigeon.swift
@@ -14,8 +14,8 @@ import SCFNotification
 #error("Pigeon doesn't support Swift versions below 5.5.")
 #endif
 
-/// Current Pigeon version 0.0.6. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
-let version = "0.0.6"
+/// Current Pigeon version 0.0.7. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
+let version = "0.0.7"
 
 public typealias Messaging = Any
 public typealias Identifier = String
